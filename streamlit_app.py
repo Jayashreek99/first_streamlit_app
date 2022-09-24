03985e847b6c5e911a00452158ac9512d594d590
Jayashree Kapoor <114111750+Jayashreek99@users.noreply.github.com>
2022-09-24 13:47:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,9 +45,10 @@ streamlit.dataframe(my_fruit_list)
     
 #streamlit.write('The user entered kiwi ', fruit_choice)
 
-#import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response)
+import requests
+fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.text(fruityvice_response)
 
 
 # write your own comment -what does the next line do? 
