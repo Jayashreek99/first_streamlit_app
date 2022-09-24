62c43f67c50edfead08a73a0b719eb619e47224c
Jayashree Kapoor <114111750+Jayashreek99@users.noreply.github.com>
2022-09-23 23:47:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,17 +45,17 @@ try:
     
 #streamlit.write('The user entered kiwi ', fruit_choice)
 
-#import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.text(fruityvice_response)
 
 
 # write your own comment -what does the next line do? 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-#streamlit.dataframe(fruityvice_normalized)
-#except URLError as e:
-#streamlit.error()
+streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+streamlit.error()
 
 streamlit.stop()
 import snowflake.connector
