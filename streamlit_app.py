d26dfb18f46016300614de4829d7b9bef1c29e01
Jayashree Kapoor <114111750+Jayashreek99@users.noreply.github.com>
2022-09-24 13:22:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,19 +24,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
-def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
+#def get_fruityvice_data(this_fruit_choice):
+#  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+#  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#  return fruityvice_normalized
 
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information")
-  else:
-     back_from_function= get_fruityvice_data(fruit_choice)
-     streamlit.dataframe(back_from_function)
+#streamlit.header("Fruityvice Fruit Advice!")
+#try:
+#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ # if not fruit_choice:
+ #   streamlit.error("Please select a fruit to get information")
+ # else:
+ #    back_from_function= get_fruityvice_data(fruit_choice)
+ #    streamlit.dataframe(back_from_function)
 
     
  #   streamlit.dataframe(fruityvice_normalized)
@@ -58,7 +58,7 @@ try:
 #streamlit.error()
 
 #streamlit.stop()
-import snowflake.connector
+#import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
