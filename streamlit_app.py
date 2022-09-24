bd6551404d90ea42d325a1871757096d1698cf17
Jayashree Kapoor <114111750+Jayashreek99@users.noreply.github.com>
2022-09-24 13:34:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,4 +74,9 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-
+import requests
+fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
+#streamlit.write('Thanks for adding jackfruit ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.write('Thanks for adding ', fruit_choice)
+#streamlit.text(fruityvice_response)
