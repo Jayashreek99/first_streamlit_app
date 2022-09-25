71088d71abb8197b6281cb88827dec364e3f6ab3
Jayashree Kapoor <114111750+Jayashreek99@users.noreply.github.com>
2022-09-25 01:00:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,8 +49,8 @@ except URLError as e:
    #streamlit.dataframe(back_from_function)
 
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-streamlit.write('The user entered ', fruit_choice)
+#fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#streamlit.write('The user entered ', fruit_choice)
 
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
