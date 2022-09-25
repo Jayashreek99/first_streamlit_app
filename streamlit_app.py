97aac69634b680bb35433da2e9f84507d33c9636
Jayashree Kapoor <114111750+Jayashreek99@users.noreply.github.com>
2022-09-25 11:41:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ def get_fruit_load_list():
   #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows =get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
- streamlit.stop() 
+  streamlit.stop() 
   
 #my_cur = my_cnx.cursor()
 #my_cur.execute("select * from fruit_load_list")
