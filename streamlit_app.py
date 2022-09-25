c58e0093c3abb04e88a68ba615f42f1d704aa3bb
Jayashree Kapoor <114111750+Jayashreek99@users.noreply.github.com>
2022-09-25 13:06:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,11 +78,11 @@ if streamlit.button('Get Fruit List'):
   streamlit.dataframe(my_data_rows)
   #streamlit.stop() 
   
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#my_cur = my_cnx.cursor()
+#my_cur.execute("select * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_rows)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
