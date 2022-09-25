24ac5fcb4242253eb926458c0b54633145816a87
Jayashree Kapoor <114111750+Jayashreek99@users.noreply.github.com>
2022-09-25 12:14:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,8 @@ def get_fruityvice_data(this_fruit_choice):
     #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     #streamlit.dataframe(fruityvice_normalised)
   
-except URLError as e:
-  streamlit.error()
+#except URLError as e:
+ #streamlit.error()
     
    #back_from_function= get_fruityvice_data(fruit_choice)
    #streamlit.dataframe(back_from_function)
